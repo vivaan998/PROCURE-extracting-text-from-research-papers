3852052e81c71f73ab60657e434e58e623a97424
Akash Amin <38359324+akashamin01@users.noreply.github.com>
2019-03-03 08:43:55 +0530

    Update testpdfextraction.py

--- testpdfextraction.py
+++ testpdfextraction.py
@@ -1,5 +1,5 @@
 import pyPDF2
-pdf_file = open('C:\\Users\\Desktop\\demo.pdf', 'rb')
+pdf_file = open('C:\\Users\\Desktop\\demo.pdf', 'rt')
 read_pdf = PyPDF2.PdfFileReader(pdf_file)
 number_of_pages = read_pdf.getNumPages()
 page = read_pdf.getPage(0)
